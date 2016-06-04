88b9b63c0a93e86bac9bfad5c6306623ebf91cfd
devnull <tilmanbeck90@gmail.com>
2016-06-04 18:04:52 +0200

    - added enum and refactored niceTree

--- NiceTree.py
+++ NiceTree.py
@@ -1,4 +1,5 @@
 import Tree
+from enum import Enum
 
 def leaf(tree):
     if(tree.getLeft() != None):
@@ -7,26 +8,32 @@ def leaf(tree):
     if(tree.getRight() != None):
         return leaf(tree.getRight())
 
-    if(not bag):
-        tree.left = createLeaf(bag)
-    pass
+    if(not tree.getBag()):
+        tree.left = createLeaf(tree.getBag())
+        return
 
 def createLeaf(bag):
     if(not bag):
-        return NiceTree(createLeaf(bag[-1]),None,bag,'I')
-    return NiceTree(None,None,[],'Leaf')
+        return NiceTree(createLeaf(bag[-1]), None, bag, BagType.IV)
+    return NiceTree(None, None, [], BagType.L)
 
-def convertToNiceTree():
-    pass
-
+#def convertToNiceTree():
 
 class NiceTree(Tree):
-    def __init__(self, left=None, right=None, bag=[], type=''):
+    def __init__(self, left=None, right=None, bag=[], bagType=None):
         Tree.__init__(left,right,bag)
-        convertToNiceTree()
-        self.type = type
-        
+        #convertToNiceTree()
+        self.bagType = bagType
+
     def setType(self,type):
         self.type = type
-    def getType(self):
-        return self.type
+    def getBagType(self):
+        return self.bagType
+
+class BagType(Enum):
+    IV = 'introduce vertex'
+    IE = 'introduce edge'
+    J  = 'join'
+    F  = 'forget'
+    L  = 'leaf'
+    R  = 'root'
