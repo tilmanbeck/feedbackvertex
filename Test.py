644322b2be84721de358a064d69f00060063c45e
devnull <tilmanbeck90@gmail.com>
2016-06-08 17:47:45 +0200

    added example graph

--- Test.py
+++ Test.py
@@ -5,14 +5,18 @@ from NiceTree import root
 from NiceTree import join
 from NiceTree import hasNoSpecialName
 
-edges = [{}, {}]
+# example graph G
+vertices = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+edges = [{'a', 'b'}, {'a', 'g'}, {'b', 'g'}, {'b', 'c'},
+         {'c', 'e'}, {'g', 'e'}, {'g', 'f'}, {'e', 'f'},
+         {'c', 'd'}, {'d', 'e'}]
 
-ecd = NiceTree(None,None,['e','c','d'])
+ecd = NiceTree(None,None,['e', 'c', 'd'])
 efg = NiceTree(None,None,['e', 'f', 'g'])
 abg = NiceTree(None,None,['a', 'b', 'g'])
-ecg = NiceTree(efg,ecd,['e','c','g'])
-bcg = NiceTree(abg,ecg,['b','c','g'])
-bc = NiceTree(bcg, None,['b','c'])
+ecg = NiceTree(efg,ecd,['e', 'c', 'g'])
+bcg = NiceTree(abg,ecg,['b', 'c', 'g'])
+bc = NiceTree(bcg, None,['b', 'c'])
 
 #treeDecomp = [rootNode1, rootNode2]
 
